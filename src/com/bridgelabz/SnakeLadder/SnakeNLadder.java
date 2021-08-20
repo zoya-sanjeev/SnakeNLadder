79e5918875c33d7a501f234b09c013062d4ec8e3
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-20 19:31:25 +0530

    Player at position 0

--- src/com/bridgelabz/SnakeLadder/SnakeNLadder.java
+++ src/com/bridgelabz/SnakeLadder/SnakeNLadder.java
@@ -1,10 +1,13 @@
 package com.bridgelabz.SnakeLadder;
 
+import java.util.Random;
+
 public class SnakeNLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to snake and ladder");
+		int pos=0;
+		System.out.println("Player is at position:"+ pos );
 
 	}
 
