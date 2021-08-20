9522e7e6b878f919cc9b19b63edd4d481dc2d316
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-20 21:50:38 +0530

    UC3 moves for snake, ladder and no play updated

--- src/com/bridgelabz/SnakeLadder/SnakeNLadder.java
+++ src/com/bridgelabz/SnakeLadder/SnakeNLadder.java
@@ -1,18 +1,27 @@
 package com.bridgelabz.SnakeLadder;
-
+import java.util.Random;
 
 public class SnakeNLadder {
-	
+	public static final int NO_PLAY=0;
+	public static final int LADDER=1;
+	public static final int SNAKE=2;
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
-		int pos=(int)Math.ceil(Math.random()*6);
-		System.out.println(pos);
+		int pos=0, roll=0;
+		roll=(int)Math.ceil(Math.random()*6);
 		
+		Random random = new Random();
+		int option=random.nextInt(3);
+		if(option==NO_PLAY)
+			pos=pos;
+		else if(option==LADDER)
+			pos+=roll;
+		else
+			pos-=roll;
 			
-		
+		System.out.println(pos);
 	}
 
 }
