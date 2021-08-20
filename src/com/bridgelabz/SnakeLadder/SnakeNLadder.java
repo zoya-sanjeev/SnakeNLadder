7f064d9234df7ee1cb2be9329daf77ef3eb3b864
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-20 21:36:58 +0530

    UC2 player rolls dice to get a number between 1 and 6

--- src/com/bridgelabz/SnakeLadder/SnakeNLadder.java
+++ src/com/bridgelabz/SnakeLadder/SnakeNLadder.java
@@ -1,34 +1,18 @@
 package com.bridgelabz.SnakeLadder;
 
-import java.util.Random;
 
 public class SnakeNLadder {
-	public static final int NO_PLAY=0;
-	public static final int LADDER=1;
-	public static final int SNAKE=2;
+	
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Random random = new Random();
-		int pos=0, turn =0, option=3;
+	
+		int pos=(int)Math.ceil(Math.random()*6);
+		System.out.println(pos);
 		
-		while(pos!=100) {
-			turn = random.nextInt(7);
-			if(turn==0)
-				continue;
-			option=random.nextInt(3);
 			
-			if(option==LADDER)
-				pos=pos+turn>100?pos:pos+turn;
-			else if(option==SNAKE)
-				pos=pos-turn<0?0:pos-turn;
-			else
-				pos=pos;
-			
-			System.out.print(pos+" ");
-			
-		}
+		
 	}
 
 }
