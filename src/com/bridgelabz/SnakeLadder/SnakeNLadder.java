1f102f75fc5dae88f3f2dfb4c975a8aa9eba393e
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-20 17:33:31 +0530

    Welcome message

---
+++ src/com/bridgelabz/SnakeLadder/SnakeNLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.SnakeLadder;
+
+public class SnakeNLadder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to snake and ladder");
+
+	}
+
+}
