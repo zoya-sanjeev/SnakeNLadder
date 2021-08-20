6f6109eae2c78b8b1f1cdc98c590b3d9aab74b4d
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-20 22:04:22 +0530

    UC4 play game till player reaches position 100

--- src/com/bridgelabz/SnakeLadder/SnakeNLadder.java
+++ src/com/bridgelabz/SnakeLadder/SnakeNLadder.java
@@ -1,5 +1,5 @@
 package com.bridgelabz.SnakeLadder;
-import java.util.Random;
+
 
 public class SnakeNLadder {
 	public static final int NO_PLAY=0;
@@ -9,19 +9,20 @@ public class SnakeNLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int pos=0, roll=0;
-		roll=(int)Math.ceil(Math.random()*6);
-		
-		Random random = new Random();
-		int option=random.nextInt(3);
+		int pos=0, roll=0, option=0;
+		while(pos<=100) {
+			roll=(int)Math.floor(Math.random()*6)+1;
+			option=(int)Math.floor(Math.random()*3);
+			
 		if(option==NO_PLAY)
-			pos=pos;
+			;
 		else if(option==LADDER)
 			pos+=roll;
 		else
 			pos-=roll;
 			
 		System.out.println(pos);
+		}
 	}
 
 }
