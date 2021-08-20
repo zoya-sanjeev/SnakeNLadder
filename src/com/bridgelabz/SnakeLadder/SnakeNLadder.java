69d782607c4ea9e82f21fbf55d06262be1abd426
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-20 20:03:37 +0530

    UC3 snake n ladder

--- src/com/bridgelabz/SnakeLadder/SnakeNLadder.java
+++ src/com/bridgelabz/SnakeLadder/SnakeNLadder.java
@@ -3,19 +3,28 @@ package com.bridgelabz.SnakeLadder;
 import java.util.Random;
 
 public class SnakeNLadder {
+	public static final int NO_PLAY=0;
+	public static final int LADDER=1;
+	public static final int SNAKE=2;
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Random random = new Random();
-		int pos=0, turn =0;
+		int pos=0, turn =0, option=3;
 		
 		while(pos!=100) {
 			turn = random.nextInt(7);
-			
 			if(turn==0)
 				continue;
+			option=random.nextInt(3);
 			
-			pos=pos+turn>100?pos:pos+turn;
+			if(option==LADDER)
+				pos=pos+turn>100?pos:pos+turn;
+			else if(option==SNAKE)
+				pos=pos-turn<0?0:pos-turn;
+			else
+				pos=pos;
 			
 			System.out.print(pos+" ");
 			
